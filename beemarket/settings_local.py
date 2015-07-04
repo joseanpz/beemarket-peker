d9091ce7f282f08559b845404f5b5130e4e52b3e
jose <backend2.muchaweb@gmail.com>
2015-07-03 19:42:57 -0500

    add local

---
+++ beemarket/settings_local.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.local',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'ATOMIC_REQUESTS': True,
+    }
+}
