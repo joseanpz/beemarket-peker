651307d59974cc81d11d2bb0e5b6b228886af0b9
Jose <backend2.muchaweb@gmail.com>
2015-07-07 16:02:43 -0500

    fix peker basket

--- basket/views.py
+++ basket/views.py
@@ -1,8 +1,26 @@
-from oscar.apps.basket.views import BasketAddView as CoreBasketAddView
+import json
+
+from django.template import RequestContext
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+
+from oscar.apps.basket.views import BasketAddView as CoreBasketAddView, BasketView as CoreBasketView
 from django.utils.http import is_safe_url
 from oscar.core.utils import safe_referrer
 from django.core.urlresolvers import reverse
 
+#
+class BasketView(CoreBasketView):
+    def json_response(self, ctx, flash_messages):
+        print 'eco json response'
+        basket_html = render_to_string(
+            'basket/partials/peker_basket_content.html',
+            RequestContext(self.request, ctx))
+        payload = {
+            'content_html': basket_html,
+            'messages': flash_messages.as_dict()}
+        return HttpResponse(json.dumps(payload),
+                             content_type="application/json")
 
 class BasketAddView(CoreBasketAddView):
 
