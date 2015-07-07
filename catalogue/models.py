a4cea5de55770579069c125cbf1b94d6f252bc1f
mothership <spock@motherships-iMac.local>
2015-07-07 12:37:30 -0500

    basket content

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,1 +1,39 @@
+from oscar.apps.catalogue.abstract_models import  AbstractProduct
+
+
+class Product(AbstractProduct):
+
+    def primary_image(self):
+        """
+        Returns the primary image for a product. Usually used when one can
+        only display one product image, e.g. in a list of products.
+        """
+        #print 'overriden pirmary image'
+        
+        if self.is_child:
+        	images = self.parent.images.all()
+        else:
+        	images = self.images.all()
+        	
+        ordering = self.images.model.Meta.ordering
+        if not ordering or ordering[0] != 'display_order':
+            # Only apply order_by() if a custom model doesn't use default
+            # ordering. Applying order_by() busts the prefetch cache of
+            # the ProductManager
+            images = images.order_by('display_order')
+        try:
+            return images[0]
+        except IndexError:
+            # We return a dict with fields that mirror the key properties of
+            # the ProductImage class so this missing image can be used
+            # interchangeably in templates.  Strategy pattern ftw!
+            return {
+                'original': self.get_missing_image(),
+                'caption': '',
+                'is_missing': True}
+
+
+
+
+
 from oscar.apps.catalogue.models import *
